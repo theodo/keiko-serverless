a530c8479c38d53f5e1f80752be58855f1928304
Mamadou <mamadoud@theodo.fr>
2022-04-21 17:23:10 +0200

    create nft handler updated

--- packages/backend/functions/createNft/handler.ts
+++ packages/backend/functions/createNft/handler.ts
@@ -1,5 +1,23 @@
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import crypto from 'crypto';
 
+const client = new DynamoDBClient({ region: 'eu-west-1' });
 
-export const main = (): void => {
-  console.log("nft created !")
+const randomIntFromInterval = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+export const main = async (): Promise<any> => {
+  console.log(event);
+  const id = crypto.randomUUID();
+  const params = {
+    TableName: process.env.NFT_TABLE_NAME,
+    Item: {
+      PK: { S: 'NFT' },
+      SK: { S: id },
+      positionX: { S: randomIntFromInterval(5, 90).toString() },
+      positionY: { S: randomIntFromInterval(10, 90).toString() },
+      imageIndex: { S: Math.floor(Math.random() * 5).toString() },
+    },
+  };
+  await client.send(new PutItemCommand(params));
 };
