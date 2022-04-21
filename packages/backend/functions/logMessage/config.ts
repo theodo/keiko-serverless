fae9b368cb012f6e2c1f6959aaee8e5eb15e303d
Mamadou <mamadoud@theodo.fr>
2022-04-21 16:41:14 +0200

    end of setup

--- packages/backend/functions/logMessage/config.ts
+++ packages/backend/functions/logMessage/config.ts
@@ -2,4 +2,12 @@ import { getHandlerPath } from '@libs/configHelper/getHandlerPath';
 
 export const logMessage = {
   handler: getHandlerPath(__dirname),
+  events: [
+    {
+      httpApi: {
+        method: 'get',
+        path: '/logMessage',
+      },
+    },
+  ],
 };
