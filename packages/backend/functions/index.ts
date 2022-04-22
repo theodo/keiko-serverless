d55ab35c54379536e87348e0162a7395826721e7
Mamadou <mamadoud@theodo.fr>
2022-04-22 09:46:19 +0200

    delete api updated

--- packages/backend/functions/index.ts
+++ packages/backend/functions/index.ts
@@ -1,5 +1,6 @@
 import { getNft } from './getNft/config';
 import { logMessage } from './logMessage/config';
 import { createNft } from './createNft/config';
+import { deleteNft } from './deleteNft/config';
 
-export const functions = { getNft, logMessage,createNft };
+export const functions = { getNft, logMessage,createNft, deleteNft };
