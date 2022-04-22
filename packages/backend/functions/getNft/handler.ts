eb949e1b3ac1efd13799fb3a940c34a8d42c6361
valentinbeggi <valentinbe@theodo.fr>
2022-04-22 16:13:59 +0200

    :coffin: remove dynamo db toolbox

--- packages/backend/functions/getNft/handler.ts
+++ packages/backend/functions/getNft/handler.ts
@@ -1,7 +1,18 @@
-import { NFTEntity } from "@libs/dynamodb-toolbox/nftEntity";
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
-export const main = async (): Promise<string> => {
-  const { Items = [] } = await NFTEntity.query("Nft");
+const client = new DynamoDBClient({ region: 'eu-west-1' });
 
-  return Items;
+export const main = async (): Promise<any> => {
+  const params = {
+    TableName: process.env.NFT_TABLE_NAME,
+    ExpressionAttributeValues: {
+      ':pk': { S: 'Nft' },
+    },
+    KeyConditionExpression: 'PK = :pk ',
+  };
+
+  const { Items = [] as any } = await client.send(new QueryCommand(params));
+
+  return Items.map(unmarshall);
 };
