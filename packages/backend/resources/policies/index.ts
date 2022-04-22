847147adeecd89d2a40002381596c8d8d26acc53
Mamadou <mamadoud@theodo.fr>
2022-04-22 09:32:12 +0200

    delete api added

--- packages/backend/resources/policies/index.ts
+++ packages/backend/resources/policies/index.ts
@@ -11,3 +11,9 @@ export const nftTableDynamoDBWritePolicies = {
   Resource: [tableArn],
   Action: ['dynamodb:PutItem', 'dynamodb:Query'],
 };
+
+export const nftTableDynamoDBDeletePolicies = {
+  Effect: 'Allow',
+  Resource: [tableArn],
+  Action: ['dynamodb:DeleteItem'],
+}
