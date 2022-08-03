a28665219d16024a2171d38585461bac2d23d279
Elias Tazartes <elias.tazartes@gmail.com>
2022-08-03 12:13:54 +0200

    fix: remove provisioned billing mode on dynamodb table and add destroy removal policy

--- packages/backend/resources/index.ts
+++ packages/backend/resources/index.ts
@@ -1,5 +1,5 @@
-import { App, Stack } from '@aws-cdk/core';
-import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
+import { App, RemovalPolicy, Stack } from '@aws-cdk/core';
+import { AttributeType, Table, BillingMode } from '@aws-cdk/aws-dynamodb';
 
 import { CloudFormationTemplate } from 'libs/configHelper/cloudformation';
 import { PARTITION_KEY, SORT_KEY } from './dynamoDB';
@@ -10,6 +10,8 @@ const stack = new Stack(app);
 const table = new Table(stack, 'NFTtable', {
   partitionKey: { name: PARTITION_KEY, type: AttributeType.STRING },
   sortKey: { name: SORT_KEY, type: AttributeType.STRING },
+  billingMode: BillingMode.PAY_PER_REQUEST,
+  removalPolicy: RemovalPolicy.DESTROY,
 });
 
 export const tableArn = stack.resolve(table.tableArn);
